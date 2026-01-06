693d285d29f46b1dbf01f441323f198562c5296a
Mark Richardson <markrichardson325@gmail.com>
2026-01-06 17:04:13 +0000

    Clean upload of final code

---
+++ current-weather/class.current-weather-api.php
@@ -0,0 +1,21 @@
+<?php
+
+class Markweather_api {
+
+    private $API_KEY;
+    private $cache_key = 'weather_data';
+    
+    function get_current_weather($location){
+        $weather_data = get_transient($this->cache_key);
+        if( false === $weather_data){
+            $this->API_KEY = get_option('currentweather-apikey');
+            $url = 'https://api.openweathermap.org/data/2.5/weather?q='.$location.'&appid='.$this->API_KEY.'&units=metric';
+            $args = array();
+            $response = wp_remote_get($url, $args);
+            $weather_data = $response["body"];
+            set_transient($this->cache_key, $weather_data, 1 * HOUR_IN_SECONDS);
+        }
+        
+        return $weather_data;
+    }
+}
